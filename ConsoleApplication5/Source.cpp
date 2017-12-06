a626c094d461e1e650831cdf75edcc4bb476b402
ГерценЕ <@227_TEACHER>
2017-12-06 09:34:31 +0600

    Add project files.

---
+++ ConsoleApplication5/Source.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	int x;
+	cin >> x;
+
+	switch (x)
+	{
+		case 6:
+		{
+			switch (x)
+			{
+			default:
+				break;
+			}
+			if (x > 0)
+			{
+
+			}
+			else
+			{
+
+			}
+		}
+		break;
+
+		case 66:
+		{
+			cout << "x = 6";
+		}
+		break;
+
+		case 666:
+		{
+			cout << "x = 6";
+		}
+		break;
+
+		case 6666:
+		{
+			cout << "x = 6";
+		}
+		break;
+
+		//else
+		default:
+		{
+
+		}
+		break;
+	}
+
+
+
+
+}
